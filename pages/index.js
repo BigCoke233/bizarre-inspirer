48ca6a75032a96085fc400fac1199a312ab6946f
Eltrac Koalar <hi@guhub.cn>
2022-11-28 13:45:08 +0800

    support random library

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,12 @@ export default function Home(data) {
    */
   //默认设置
   var settings = {
-    "model": "AC-phrase",
+    "model": null,
     "custom_model": []
   };
   //词组模型设置
   var modelList = Object.keys(data.data.libs);
-  const modelOptions = []
+  const modelOptions = [{"value":"random","label":"随机模型"}]
   modelList.forEach((e)=>{
     modelOptions.push({
       "value": e,
@@ -49,16 +49,33 @@ export default function Home(data) {
       //获取词库
       var words = data.data.words[lib];
       //获取随机数
-      var range = words.length-1;
+      var range = words.length;
       var rand = Math.random();
       var num = (Math.round(rand*range));
 
       return words[num];
     }
 
+    function getRandomModel(){
+      //获取词组模型列表
+      var libs = Object.keys(data.data.libs);
+      //获取随机数
+      var range = libs.length-1;
+      var rand = Math.random();
+      var num = (Math.round(rand*range));
+
+      return libs[num];
+    }
+
+    //开始生成词组
     var output='';
     //获取词组模型
-    var model = data.data.libs[settings.model];
+    var model;
+    if(settings.model==null || settings.model=='random'){
+      model = data.data.libs[getRandomModel()]
+    }else{
+      model = data.data.libs[settings.model]
+    }
     var word_list=[]; var i=0; //用于查重去重
     //获取词组格式
     if(settings.custom_model.length==0) {
@@ -71,7 +88,7 @@ export default function Home(data) {
       var repeat = true;
       while(repeat) {
         var word = getRandomWord(e);
-        if(i!=0 && word_list[i-1]==word){
+        if(i!=0 && word_list[i-1]==word || word==undefined || word==''){
           repeat=true;
         }else{
           output += '<span>'+word+'</span>';
@@ -146,7 +163,7 @@ export default function Home(data) {
         <h1 className="font-semibold text-gray-600 text-lg my-4">怪奇灵感生成器</h1>
         <div className="font-bold text-gray-800 tracking-wider px-4">
           <p className="select-none text-3xl md:text-6xl my-2">我想到了</p>
-          <p className="text-gray-600 text-5xl font-normal md:text-6xl flex" id="inspiration">什么呢？</p>
+          <p className="text-gray-600 text-5xl font-normal md:text-6xl flex justify-center" id="inspiration">什么呢？</p>
         </div>
         <div className="my-4 md:my-8 flex gap-2 flex-row-reverse md:flex-row z-10" id="action">
           <button className="rounded-full py-2 px-3 bg-gray-100 text-gray-700 text-xl border
@@ -174,7 +191,7 @@ export default function Home(data) {
           <div id="setting-content" className="p-5 py-2 overflow-y-auto" style={{minHeight: 'calc(80vh - 3rem)'}}>
 
             <label className="text-lg my-2 block">词组模型</label>
-            <Select id="model-option" options={modelOptions}  
+            <Select id="model-option" options={modelOptions} defaultValue={modelOptions[0]} 
             onChange={(choice)=>{settings.model=choice.value}} placeholder="选择一个词组模型..." />
             <p className="text-gray-600 my-2 text-sm">决定生成什么样的词组或句子。若自定义模型设置项不为空，则此项不生效。</p>
 
